5fef06fc9ba934b07dd28d9bb80d8ecd84adb111
monate0615 <monate615@gmail.com>
2024-11-18 08:57:26 -0500

    fix 3

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,11 @@
-cd ./MONATE.Web.Server
+cd ./monate.web.client
+npm install
+
+cd ../MONATE.Web.Server
+apt-get update
+apt-get install -y dotnet-sdk-8.0
+apt-get install -y dotnet-runtime-8.0
+apt-get install -y aspnetcore-runtime-8.0
+
+dotnet restore
 dotnet run
